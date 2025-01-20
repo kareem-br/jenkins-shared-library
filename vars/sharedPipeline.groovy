3d5a271e1755304e86b7dd68046cf4e9928260dd
kareem-br <kareem.m@blackrocktechnologies.co.uk>
2025-01-20 15:47:29 +0200

    added unit test stage

--- vars/sharedPipeline.groovy
+++ vars/sharedPipeline.groovy
@@ -27,9 +27,8 @@ def call(Map config = [:]) {
             stage('INJECTING ENV FILES') {
                 steps {
                     script {
-                        def initialMsg = slackSend(channel: SLACK_CHANNEL, color: '#808080', message: "Pipeline started for ${env.JOB_NAME} - Build #${env.BUILD_NUMBER}")
-                        def threadTs = initialMsg.threadId // Capture the thread timestamp
-                        slackSend(channel: SLACK_CHANNEL, threadId: threadTs, message: "🔄 Injecting environment files...")
+                        def slackResponse = slackSend(channel: SLACK_CHANNEL, color: '#808080', message: "Pipeline started for ${env.JOB_NAME} - Build #${env.BUILD_NUMBER}")
+                        slackSend(channel: slackResponse.threadId, message: "🔄 Injecting environment files...")
                         container(DOCKER_AGENT) {
                             withCredentials([usernamePassword(credentialsId: "devops-github-token", usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
                                 sh """
@@ -39,7 +38,7 @@ def call(Map config = [:]) {
                                 """
                             }
                         }
-                        slackSend(channel: SLACK_CHANNEL, threadId: threadTs, message: "✅ Environment files injected.")
+                        slackSend(channel: slackResponse.threadId, message: "✅ Environment files injected.")
                     }
                 }
             }
